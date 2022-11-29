aba0da84e42bc0fc685c506df562eb181e70654d
arif1018 <arif.razzaque.khi@gmail.com>
2022-11-29 17:18:59 +0500

    server

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect("mongodb://arif:admin@18.210.167.222:27017/css")
         console.log(`Database Has Been Connected ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
